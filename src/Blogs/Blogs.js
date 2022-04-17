b14b704f946b09db5b1cd92b7d2f6b6d7282335b
NirabHossain <nirabamth@gmail.com>
2022-04-18 00:46:11 +0600

    blogs

--- src/Blogs/Blogs.js
+++ src/Blogs/Blogs.js
@@ -2,15 +2,33 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
+        <div className='p-5'>
             <h3>What is the difference between authentication and authorization?</h3>
             <p>
-                While authentication and authorization are often used interchangeably, they are separate processes used to protect an organization from cyber-attacks. As data breaches continue to escalate in both frequency and scope, authentication and authorization are the first line of defense to prevent confidential data from falling into the wrong hands. As a result, strong authentication and authorization methods should be a critical part of every organization’s overall security strategy.
+                While authentication and authorization are often used interchangeably, they are separate processes used to protect an organization from cyber-attacks. 
+
+                In a word, authentication is done first and then authorization comes. Authentication is mainly done by user while authorization is controlled by developers
+                Authentication verifies who the user is.	Authorization determines what resources a user can access.
+
+                Verifying email address to take money from digital payment is an example of authentication.
+                Giving someone permission to download a particular file on a server or <strong>making someone admin in a particular facebook page</strong> are good examples of authorization.
             </p>
-            <h3></h3>
-            <p></p>
-            <p></p>
-        </div>
+            <h3>Why are you using firebase? What other options do you have to implement authentication?</h3>
+            <p>
+                Actually authentication requires password security to make secure connection with users. If user's password can be known from a system, that might harm to users because of being able to do anything with that password. Google provides <strong>firebase</strong> system to help us protecting the user information. There are many systems where we can store our user information, not just google. Also we can use our own authentication system using <strong> cryptographic algorithms</strong>. For reliability, firebase is a good choice to put our trust
+            </p>
+            <h3>What other services do firebase provide?</h3>
+            <p>
+                There are many services of firebase other than just authentication. Some of these are listed as follows:
+                <ul>
+                    <li>Cloud Firestore </li>
+                    <li>Remote Config</li>
+                    <li>Firebase ML</li>
+                    <li>Cloud Messaging</li>
+                    <li>Hosting</li>
+                </ul>
+            </p>
+        </div >
     );
 };
 
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -10,6 +10,9 @@ const About = () => {
 
 
             <p className='bg-info w-50 mx-auto p-5'>
+
+                I am currently finishing my Master's degree and most probably try to go abroad to take a PHD degree from an university. Beside pursuing the degree I want myself to be a web developer so that I can earn remotely. And my future goal is to make a company to make some job sector for the juveniles of our country as much as I can. For this reason I am working hard. I am quite good in mathematics which helps me to program finely. That ignites me to do even more. By this year, I want to see myself as a full-stack developer. And by 5 years I want to level up as a owner. 
+
                 I am a frontend web developer. I use <strong>reactJS</strong> to develop a website. If you want to hire me, please check my profile through the following links:<br /><br />
 
                 <a target="blank" href="https://github.com/NirabHossain" className='text-decoration-none text-dark'>Github: <code>github.com/NirabHossain</code></a><br />
