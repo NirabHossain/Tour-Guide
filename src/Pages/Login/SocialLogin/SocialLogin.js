f14a4f77543bd1bff2741785a0ee9690868e8940
NirabHossain <nirabamth@gmail.com>
2022-04-18 21:11:55 +0600

    loading added

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -3,16 +3,18 @@ import google from '../../../images/social/google.png';
 import {useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useNavigate } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
 
 
     // Email password and social log in with google only
 
 const SocialLogin = () => {
-    const [signInWithGoogle, user, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const navigate = useNavigate();
     let errorElement;
 
 
+    if(loading)return <Loading></Loading>
     if (error) errorElement = <p className='text-danger'>Error: {error?.message}</p>
     if (user) navigate('/checkout');
 
